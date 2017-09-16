9dba99a68e3accc7b228f629fedaa96edea0faa0
BeansTown106 <nbenis106@gmail.com>
2017-09-16 16:25:46 -0400

    aosp_fixs: move ro.carrier=unknown to vendor [2/2]

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -23,4 +23,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dataroaming=false \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
-    ro.setupwizard.rotation_locked=true
+    ro.setupwizard.rotation_locked=true \
+    ro.carrier=unknown
